d872e479f6613d852549bb693a45afcd076fc36f
LoopyLucy <erinlucyfitton@outlook.com>
2025-09-22 16:03:00 +0100

    waybar now working (some modules missing)

--- waybar/waybar.nix
+++ waybar/waybar.nix
@@ -6,12 +6,41 @@
 	#mode = "createLink";
     #};
 
-    wayland.windowManager.hyprland.settings.exec-once = [
-	"waybar"
-    ];
+    wayland.windowManager.hyprland.settings.exec-once = [ "waybar" ];
 
     programs.waybar = {
-	enable = true;
-	style = ./style.css;
+        enable = true;
+        style = ./style.css;
+
+        settings = {
+            main = {
+                layer = "top";
+                position = "top";
+                width = 1070;
+                margin-left = 200;
+                margin-right = 200;
+                margin-top = 4;
+
+                modules-left = [
+                    "clock"
+                    "custom/weather"
+                    "temperature"
+                    "cpu"
+                ];
+
+                modules-center = [
+                    "hyprland/workspaces#rw"
+                ];
+
+                modules-right = [
+                    "tray"
+                    "mpris"
+                    "pulseaudio"
+                    "battery"
+                    "custom/power"
+                    "group/notify"
+                ];
+            };
+        };
     };
 }
