9320b96584382e46d6ffbf3c10519d8f106236ba
LoopyLucy <erinlucyfitton@outlook.com>
2025-09-26 03:07:30 +0100

    modules separated

---
+++ home/waybar/modules.nix
@@ -0,0 +1,108 @@
+{ ... }:
+
+{
+    programs.waybar.settings.main = {
+        "hyprland/workspaces" = {
+            all-outputs = false;
+            active-only = false;
+            on-click = "activate";
+            show-special = false;
+            on-scroll-up = "hyprctl dispatch split-workspace -1";
+            on-scroll-down = "hyprctl dispatch split-workspace +1";
+            persistent-workspaces = { "*" = 1; };
+            format = "{windows}";
+            format-icons = {
+                active = " :";
+                default = " :";
+            };
+            format-window-separator = " ";
+            window-rewrite-default = "";
+            window-rewrite = {
+                "class<kitty|konsole>" = "";
+                "class<.*zen.*>" = "󰰷";
+                "class<code|VSCode|code-url-handler|code-oss|codium|codium-url-handler|VSCodium>" = "";
+                "class<[Tt]hunderbird|[Tt]hunderbird-esr>" = "";
+                "class<[Dd]iscord|[Ww]ebcord|Vesktop>" = "";
+            };
+        };
+
+        "custom/power" = {
+            format = " ⏻ ";
+            on-click = "$HOME/.config/hypr/scripts/Wlogout.sh";
+            on-click-right = "$HOME/.config/hypr/scripts/ChangeBlur.sh";
+            tooltip = true;
+            tooltip-format = "Left Click = Logout Menu\nRight Click = Change Blur";
+        };
+
+        "custom/swaync" = {
+            tooltip = true;
+            tooltip-format = "Left Click: Launch Notification Center\nRight Click: Do not Disturb";
+            format = "{icon} ";
+            format-icons = {
+                notification = "<span foreground='red'><sup></sup></span>";
+                none = "";
+                dnd-notification = "<span foreground='red'><sup></sup></span>";
+                dnd-none = "";
+                inhibited-notification = "<span foreground='red'><sup></sup></span>";
+                inhibited-none = "";
+                dnd-inhibited-notification = "<span foreground='red'><sup></sup></span>";
+                dnd-inhibited-none = "";
+            };
+            return-type = "json";
+            exec-if = "which swaync-client";
+            exec = "swaync-client -swb";
+            on-click = "sleep 0.1 && swaync-client -t -sw";
+            on-click-right = "swaync-client -d -sw";
+            escape = true;
+        };
+
+        "temperature" = {
+            interval = 10;
+            tooltip = true;
+            hwmon-path = [
+                "/sys/class/hwmon/hwmon1/temp1_input"
+                "/sys/class/thermal/thermal_zone0/temp"
+            ];
+            #thermal-zone = 0;
+            critical-threshold = 82;
+            format-critical = "{temperatureC}°C {icon}";
+            format = "{temperatureC}°C {icon}";
+            format-icons = [
+                "󰈸"
+            ];
+            on-click-right = "$HOME/.config/hypr/scripts/WaybarScripts.sh --nvtop";
+        };
+
+        "tray" = {
+            icon-size = 20;
+            spacing = 4;
+        };
+
+        "pulseaudio" = {
+            format = "{icon} {volume}%";
+            format-bluetooth = "{icon} 󰂰 {volume}%";
+            format-muted = "󰖁";
+            format-icons = {
+                headphone = "";
+                hands-free = "";
+                headset = "";
+                phone = "";
+                portable = "";
+                car = "";
+                default = [
+                    "" "" "󰕾" ""
+                ];
+                ignored-sinks = [
+                    "Easy Effects Sink"
+                ];
+            };
+            scroll-step = 5.0;
+            on-click = "$HOME/.config/hypr/scripts/Volume.sh --toggle";
+            on-click-right = "pavucontrol -t 3";
+            on-scroll-up = "$HOME/.config/hypr/scripts/Volume.sh --inc";
+            on-scroll-down = "$HOME/.config/hypr/scripts/Volume.sh --dec";
+            tooltip-format = "{icon} {desc} | {volume}%";
+            smooth-scrolling-threshold = 1;
+        };
+    };
+}
--- home/waybar/waybar.nix
+++ home/waybar/waybar.nix
@@ -1,6 +1,9 @@
 { pkgs, lib, ... }:
 
 {
+    imports = [
+        ./modules.nix
+    ];
     #catpuccin.waybar = {
         #enable = true;
 	#mode = "createLink";
@@ -38,64 +41,8 @@
                     "pulseaudio"
                     "battery"
                     "custom/power"
-                    "group/notify"
+                    "custom/swaync"
                 ];
-
-                #Modules
-                "hyprland/workspaces" = {
-                    all-outputs = true;
-                    on-click = "activate";
-                    show-special = false;
-                    on-scroll-up = "hyprctl dispatch workspace e-1";
-                    on-scroll-down = "hyprctl dispatch workspace e+1";
-                    persistent-workspaces = { "*" = 1; };
-                    format = "{windows} ";
-                    format-icons = {
-                        active = " :";
-                        default = " :";
-                    };
-                    format-window-separator = " ";
-                    window-rewrite-default = "";
-                    window-rewrite = {
-                        "class<kitty|konsole>" = "";
-                        "class<.*zen.*>" = "󰰷";
-                        "class<code|VSCode|code-url-handler|code-oss|codium|codium-url-handler|VSCodium>" = "";
-                        "class<[Tt]hunderbird|[Tt]hunderbird-esr>" = "";
-                        "class<[Dd]iscord|[Ww]ebcord|Vesktop>" = "";
-                    };
-                };
-
-                "custom/swaync" = {
-                    tooltip = true;
-                    tooltip-format = "Left Click: Launch Notification Center\nRight Click: Do not Disturb";
-                    format = "{} {icon} ";
-                    format-icons = {
-                        notification = "<span foreground='red'><sup></sup></span>";
-                        none = "";
-                        dnd-notification = "<span foreground='red'><sup></sup></span>";
-                        dnd-none = "";
-                        inhibited-notification = "<span foreground='red'><sup></sup></span>";
-                        inhibited-none = "";
-                        dnd-inhibited-notification = "<span foreground='red'><sup></sup></span>";
-                        dnd-inhibited-none = "";
-	                };
-                    return-type = "json";
-                    exec-if = "which swaync-client";
-                    exec = "swaync-client -swb";
-                    on-click = "sleep 0.1 && swaync-client -t -sw";
-                    on-click-right = "swaync-client -d -sw";
-                    escape = true;
-                };
-
-                "group/notify" = {
-                    orientation = "inherit";
-                    drawer = {
-                        transition-duration = 500;
-                        children-class = "custom/swaync";
-                        transition-left-to-right = false;
-                    };
-                    modules = "custom/swaync";
-                };
             };
         };
     };
