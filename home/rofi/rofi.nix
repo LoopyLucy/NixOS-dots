75bee36f600c074eca75c4731d8cb2d3e5657d43
LoopyLucy <erinlucyfitton@outlook.com>
2025-09-26 15:55:05 +0100

    rofi theme 1

--- home/rofi/rofi.nix
+++ home/rofi/rofi.nix
@@ -3,17 +3,24 @@
 let
     inherit (config.lib.formats.rasi) mkLiteral;
 in {
+
+	#xdg.configFile."rofi/colours.rasi".source = ./;
+
     programs.rofi = {
     	enable = true;
+
+		theme = {
+			"@theme" = ./style.rasi;
+		};
     };
 
     wayland.windowManager.hyprland.settings = {
-	windowrule = [
-	    "stayfocused, class:^Rofi$"
-	];
-	bindr = [
-	    "$mainMod, $mainMod_L, exec, pkill rofi || rofi -show drun -modi drun,filebrowser,run,window" #single tap $mainMod key open rofi
-	];
+		windowrule = [
+			"stayfocused, class:^Rofi$"
+		];
+		bindr = [
+			"$mainMod, $mainMod_L, exec, pkill rofi || rofi -show drun -modi drun,filebrowser,run,window" #single tap $mainMod key open rofi
+		];
     };
 
 }
