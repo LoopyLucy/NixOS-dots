867d89330549aec717f48bc376a3fa2e73a1ca88
LoopyLucy <erinlucyfitton@outlook.com>
2025-10-02 02:29:32 +0100

    opcity

--- home/hyprland/hyprland.nix
+++ home/hyprland/hyprland.nix
@@ -46,8 +46,8 @@
             decoration = {
                 rounding = 10;
 
-                active_opacity = 1.0;
-                inactive_opacity = 0.9;
+                active_opacity = 0.9;
+                inactive_opacity = 0.8;
                 fullscreen_opacity = 1.0;
 
                 dim_inactive = true;
