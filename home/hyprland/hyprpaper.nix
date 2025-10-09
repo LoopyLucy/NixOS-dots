0e3c4361df76b37cdc4d137667b3af0a0dca546e
LoopyLucy <erinlucyfitton@outlook.com>
2025-10-09 17:28:37 +0100

    Background attempt

--- home/hyprland/hyprpaper.nix
+++ home/hyprland/hyprpaper.nix
@@ -4,6 +4,7 @@
     wayland.windowManager.hyprland.settings = {
         exec-once = [
             "systemctl --user start hyprpaper"
+            "bash ~/.config/hypr/scripts/RandBackground.sh"
         ];
     };
 
