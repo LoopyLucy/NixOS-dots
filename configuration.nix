0797237953d139820d86fc97a05ae3dc3333be97
LoopyLucy <erinlucyfitton@outlook.com>
2026-02-15 01:34:29 +0000

    newlines

--- configuration.nix
+++ configuration.nix
@@ -221,7 +221,7 @@
       noto-fonts-cjk-serif
       noto-fonts-color-emoji
     ];
-    
+
     fontconfig.defaultFonts = {
         serif = [ "Noto Serif" "Noto Serif CJK JP" ];
         sansSerif = [ "Noto Sans" "Noto Sans CJK JP" ];
@@ -389,11 +389,13 @@
       "https://nix-gaming.cachix.org"
       "https://nix-citizen.cachix.org"
     ];
+
     trusted-substituters = [
       "https://hyprland.cachix.org"
       "https://nix-gaming.cachix.org"
       "https://nix-citizen.cachix.org"
     ];
+    
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
