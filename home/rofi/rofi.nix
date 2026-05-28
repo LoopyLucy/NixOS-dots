498fab73d12055c5ce39e1481c7dc0109a693ca5
LoopyLucy <erinlucyfitton@outlook.com>
2026-05-28 22:36:40 +0100

    rollback

--- home/rofi/rofi.nix
+++ home/rofi/rofi.nix
@@ -5,28 +5,6 @@ let
         luaify lambda call bind_flags bind bind_exec with_flags on_startup;
     inherit (config.lib.formats.rasi) mkLiteral;
 in {
-
-	home.packages = [
-		(pkgs.writeShellScriptBin "rofi-file-launcher" ''
-			# Generate files (Adjust target directory as needed)
-			FILE_LIST=$(${pkgs.findutils}/bin/find ~ -maxdepth 3 -type f 2>/dev/null)
-		
-			# Launch rofi
-			CHOICE=$(echo "$FILE_LIST" | rofi -dmenu -p "Search Files:")
-			EXIT_STATUS=$?
-		
-			# Handle actions based on mouse clicks
-			if [ "$EXIT_STATUS" -eq 0 ] && [ -n "$CHOICE" ]; then
-				# Left-click / Enter: Open file
-				${pkgs.xdg-utils}/bin/xdg-open "$CHOICE"
-			elif [ "$EXIT_STATUS" -eq 10 ] && [ -n "$CHOICE" ]; then
-				# Right-click: Open the file's parent location
-				PARENT_DIR=$(${pkgs.findutils}/bin/dirname "$CHOICE")
-				${pkgs.xdg-utils}/bin/xdg-open "$PARENT_DIR"
-			fi
-		'')
-	];
-
 	xdg.configFile."rofi/colours.rasi".source = ./colours.rasi;
 	xdg.configFile."rofi/style.rasi".source = ./style.rasi;
 
@@ -34,11 +12,6 @@ in {
     	enable = true;
 		font = "JetBrainsMono Nerd Font SemiBold 11";
 		theme = "~/.config/rofi/style.rasi";
-		extraConfig = {
-			me-select-entry = "MousePrimary";
-			me-accept-custom = "MouseSecondary";
-			kb-custom-1 = "Control+MouseSecondary";
-  		};
     };
 
     wayland.windowManager.hyprland.settings = {
